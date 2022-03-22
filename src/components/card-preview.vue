91ed742e4be14fdbd63b29ecdea5d60829cc4857
Tamir <Tamirbelisha@gmail.com>
2022-03-22 17:52:14 +0200

    added components for list and card

---
+++ src/components/card-preview.vue
@@ -0,0 +1,23 @@
+<template>
+  <section>
+      <p>{{card.title}}</p>
+  </section>
+</template>
+
+<script>
+
+export default {
+  name: 'card-preview',
+  props: {
+    card: Object,
+  },
+  components: {},
+  created() {},
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  unmounted() {},
+}
+</script>
