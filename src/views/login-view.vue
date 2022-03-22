5e067d369aff894504178dfba7e931de9b0ad1cd
shani kupiec <shani.kupiec@gmail.com>
2022-03-22 17:15:31 +0200

    router and basic cmp

---
+++ src/views/login-view.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    login
+  </section>
+</template>
+
+<script>
+// import appHeader from './components/app-header.vue'
+
+export default {
+  name: 'cmp name',
+//  props: {
+//    car: Object,
+//  },
+  components: {},
+  created() {},
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {},
+  unmounted() {},
+}
+</script>
