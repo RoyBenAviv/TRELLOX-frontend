95530e273d4cd427646d1e1c8eb52f49e59e16d1
Roy Ben Aviv <roybenavivv@gmail.com>
2022-03-24 10:35:00 +0200

    basic fixes and header changes

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -1,9 +1,11 @@
 <template>
+  <home-header />
   <main class="home-view main-layout">
     
     <article>
       <h1>Trellox helps teams move work forward.</h1>
       <p>Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Trello.</p>
+      <button @click="toWorkSpace">Start now >></button>
     </article>
     <img src="https://res.cloudinary.com/trellox/image/upload/v1648060642/cooperation_rdrrnf.png" />
 
@@ -11,9 +13,10 @@
 </template>
  
 <script>
+import homeHeader from '../components/home/home-header.vue'
  
 export default {
- 
+
 name: 'home',
  data() {
 return {
@@ -21,10 +24,14 @@ return {
 };
  },
 methods: {
+  toWorkSpace() {
+    this.$router.push(`/workspace`)
+  }
 },
 computed: {
 },
 components: {
+  homeHeader
 }
 };
 </script>
