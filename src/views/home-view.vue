2b23656523e1e0892e146b6a69ca81d84832fb4f
Roy Ben Aviv <roybenavivv@gmail.com>
2022-03-26 23:40:51 +0300

    home page and workspace style improvements

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -5,7 +5,7 @@
     <article>
       <h1>Trellox helps teams move work forward.</h1>
       <p>Collaborate, manage projects, and reach new productivity peaks. From high rises to the home office, the way your team works is unique—accomplish it all with Trello.</p>
-      <button @click="toWorkSpace">Start now >></button>
+      <button @click="toWorkSpace">Start now <span><i class="fa-solid fa-arrow-right"></i></span></button>
     </article>
     <img src="https://res.cloudinary.com/trellox/image/upload/v1648060642/cooperation_rdrrnf.png" />
 
