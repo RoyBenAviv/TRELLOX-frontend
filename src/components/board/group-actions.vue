c7f01a4b5394a43e991d4c38057b29fb97ec8e7d
Roy Ben Aviv <roybenavivv@gmail.com>
2022-03-26 19:13:10 +0300

    group actions improvements

--- src/components/board/group-actions.vue
+++ src/components/board/group-actions.vue
@@ -4,62 +4,61 @@
       <p>List actions</p>
       <span @click="closeGrpAct"><i class="fa-solid fa-xmark"></i></span>
     </div>
-    <ul>
-      <li @click="addCard"><a>Add card...</a></li>
-      <li @click="openCopyGroup = true">
-        <a>Copy list...</a>
+    <div v-if="!openCopyGroup && !openMoveGroup && !openMoveCards">
+      <ul>
+        <li @click="addCard"><a>Add card...</a></li>
+        <li @click.stop="openCopyGroup = true"><a>Copy list...</a></li>
+        <li @click.stop="openMoveGroup = true"><a>Move list...</a></li>
+      </ul>
+      <ul>
+        <hr />
+        <li><a>Sort By...</a></li>
+        <hr />
+      </ul>
+      <ul>
+        <li @click.stop="openMoveCards = true"><a>Move all cards in this list...</a></li>
+        <li @click="archiveCards"><a>Archive all cards in this list...</a></li>
+      </ul>
+      <ul>
+        <hr />
+        <li @click="archiveGroup"><a>Archive this list</a></li>
+      </ul>
+    </div>
+    <div class="copy-group" v-if="openCopyGroup">
+    
+      <input v-focus v-model="groupTitle" type="text" placeholder="Enter list title"/>
+      <button @click="copyGroup">Create list</button>
+    </div>
 
-        <div v-if="openCopyGroup">
-          <input v-model="groupTitle" type="text" />
-          <button @click="copyGroup"></button>
-        </div>
-      </li>
-      <li @click="openMoveGroup = true">
-      <a>Move list...</a>
-      
-        <div v-if="openMoveGroup">
-            <form @submit.prevent="moveGroup">
-
+    <div class="move-group" v-if="openMoveGroup">
+      <form @submit.prevent="moveGroup">
+        <label>
+          <div class="board-info">
+            <p>Board</p>
+            <p>{{ chosenBoard.title }}</p>
             <select v-model="chosenBoard">
-                <!-- <optgroup label="Boards">
-                    <option selected="selected">{{chosenBoard.title}}</option>
-                </optgroup> -->
-                <option v-for="board in boards" :key="board._id" :value="board">{{board.title}}</option>
+              <option v-for="board in boards" :key="board._id" :value="board">{{ board.title }}</option>
             </select>
+          </div>
+        </label>
+        <label>
+          <div class="board-info">
+            <p>Position</p>
+            <p>{{ groupPos + 1 }}</p>
             <select v-model="groupPos">
-                <option v-for="(group, idx) in chosenBoard.groups" :key="group" :value="idx">{{idx + 1}}</option>
-
+              <option v-for="(group, idx) in chosenBoard.groups" :key="group" :value="idx">{{ idx + 1 }}</option>
             </select>
-            <button>Save</button>
-            </form>
-            
+          </div>
+        </label>
+        <button>Save</button>
+      </form>
+    </div>
 
-        </div>
-      </li>
-    </ul>
-    <ul>
-      <hr />
-      <li><a>Sort By...</a></li>
-      <hr />
-    </ul>
-    <ul>
-      <li @click="openMoveCards = true"><a>Move all cards in this list...</a>
-      
-        <div v-if="openMoveCards">
-            <select v-model="chosenGroup" @change="moveAllCards">
-                <option v-for="group in currentGroups" :key="group.id" :value="group">{{group.title}}</option>
-
-
-            </select>
-        </div>
-      
-      </li>
-      <li @click="archiveCards"><a>Archive all cards in this list...</a></li>
-    </ul>
-    <ul>
-      <hr />
-      <li @click="archiveGroup"><a>Archive this list</a></li>
-    </ul>
+    <div v-if="openMoveCards">
+      <ul>
+        <li @click="moveAllCards(group)" v-for="group in currentGroups" :key="group.id">{{group.title}}</li>
+      </ul>
+    </div>
   </section>
 </template>
 
@@ -74,8 +73,7 @@ export default {
       openMoveCards: false,
       currentGroups: this.$store.getters.currBoard.groups,
       chosenBoard: this.$store.getters.currBoard,
-      chosenGroup: null,
-      groupPos: null
+      groupPos: null,
     }
   },
   methods: {
@@ -90,18 +88,18 @@ export default {
       this.groupTitle = ''
     },
     moveGroup() {
-        const moveToBoard = this.boards.find(board => board._id === this.chosenBoard._id)
-        this.$emit('moveGroup', moveToBoard, this.groupPos)
+      const moveToBoard = this.boards.find((board) => board._id === this.chosenBoard._id)
+      this.$emit('moveGroup', moveToBoard, this.groupPos)
     },
-    moveAllCards() {
-        this.$emit('moveAllCards', this.chosenGroup)
+    moveAllCards(group) {
+      this.$emit('moveAllCards', group)
     },
     archiveCards() {
       this.$emit('archiveCards')
     },
     closeGrpAct() {
       this.$emit('closeGrpAct')
-    }
+    },
   },
   computed: {
     boards() {
