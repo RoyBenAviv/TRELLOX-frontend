135d65eb3ff07c0162990aa6d0dc22802233cd65
Tamir <Tamirbelisha@gmail.com>
2022-03-28 22:02:02 +0300

    action buttons

--- src/components/board/quick-edit-actions.vue
+++ src/components/board/quick-edit-actions.vue
@@ -4,31 +4,31 @@
             <span class="icon card"></span>
             <span class="txt">Open card</span>
          </div>
-         <div @click="openCmp('label-picker')" class="action-button">
+         <div @click="openModal('label-picker')" class="action-button">
             <span class="icon label"></span>
             <span class="txt">Edit labels</span>
          </div>
-         <div @click="openCmp('member-picker')" class="action-button">
+         <div @click="openModal('member-picker')" class="action-button">
             <span class="icon member"></span>
             <span class="txt">Change members</span>
          </div>
-         <div @click="openCmp('cover-picker')" class="action-button">
+         <div @click="openModal('cover-picker')" class="action-button">
             <span class="icon cover"></span>
             <span class="txt">Change cover</span>
          </div>
-         <div @click="openCmp('move-place-picker')" class="action-button">
+         <div @click="openModal('move-place-picker')" class="action-button">
             <span class="icon move"></span>
             <span class="txt">Move</span>
          </div>
-         <div @click="openCmp('copy-place-picker')" class="action-button">
+         <div @click="openModal('copy-place-picker')" class="action-button">
             <span class="icon copy"></span>
             <span class="txt">Copy</span>
          </div>
-         <div @click="openCmp('date-picker')" class="action-button">
+         <div @click="openModal('date-picker')" class="action-button">
             <span class="icon date"></span>
             <span class="txt">Edit dates</span>
          </div>
-         <div @click="$emit('removeCard')" class="action-button">
+         <div @click="openModal('confirm-delete')" class="action-button">
             <span class="icon remove"></span>
             <span class="txt">Delete</span>
          </div>
@@ -46,6 +46,9 @@ return {
 };
  },
 methods: {
+    openModal(cmpName) {
+        this.$emit('openModal', cmpName)
+    }
 },
 computed: {
 },
