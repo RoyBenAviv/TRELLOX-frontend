c98ffcf56c5556465f63e5e1bc139a98afca77d8
Roy Ben Aviv <roybenavivv@gmail.com>
2022-03-29 21:34:59 +0300

    added date support

--- src/components/board/date-picker.vue
+++ src/components/board/date-picker.vue
@@ -1,9 +1,12 @@
 <template>
-  <custom-modal @closeModal="closeModal">
+  <custom-modal class="date-picker" @closeModal="closeModal">
     <template v-slot:header> Dates </template>
+    <div class="calendar-container">
+      <DatePicker v-model="date" />
+    </div>
 
-    <label for="reminder">Set due date reminder</label>
-    <select name="reminder">
+    <!-- <label>Set due date reminder
+    <select>
       <option>None</option>
       <option>At time of due date</option>
       <option>5 Minutes before</option>
@@ -14,46 +17,64 @@
       <option>1 Day before</option>
       <option>2 Day before</option>
     </select>
+    </label> -->
+    <h4>Due date</h4>
+    <div class="date-options">
+      <input disabled type="date" v-model="readDate" class="custom-input" />
+      <input type="time" class="custom-input" v-model="time" />
+    </div>
+
     <h4>Reminders will be sent to all members and watchers of this card.</h4>
-    <button class="custom-btn">Save</button>
-    <button class="custom-btn">Remove</button>
+    <button @click="setDate" class="custom-btn save">Save</button>
+    <button @click="removeDate" class="custom-btn">Remove</button>
   </custom-modal>
 </template>
 
 <script>
 import customModal from './custom-modal.vue'
-
+import { Calendar, DatePicker } from 'v-calendar'
 export default {
   props: {
     card: Object,
   },
   components: {
     customModal,
+    Calendar,
+    DatePicker,
   },
   created() {},
   data() {
     return {
-      newMember: null,
-      memberIds: JSON.parse(JSON.stringify(this.card.memberIds)),
+      date: new Date(),
+      time: null
     }
   },
   methods: {
     closeModal() {
       this.$emit('closeModal')
     },
-    toggleMember(memberId) {
-      const idx = this.memberIds.findIndex((mId) => mId === memberId)
-      if (idx === -1) this.memberIds.push(memberId)
-      else this.memberIds.splice(idx, 1)
-      this.save()
+    setDate() {
+      if(!this.time) this.time = '00:00'
+      const dueDate = {
+        date: this.date,
+        time: this.time,
+        isCompleted: null,
+      }
+      this.$emit('updateKey', 'dueDate', JSON.parse(JSON.stringify(dueDate)))
     },
-    save() {
-      this.$emit('updateKey', 'memberIds', JSON.parse(JSON.stringify(this.memberIds)))
+    removeDate() {
+      const dueDate = null
+      this.$emit('updateKey', 'dueDate', JSON.parse(JSON.stringify(dueDate)))
     },
   },
   computed: {
-    members() {
-      return this.$store.getters.currBoard.members
+    readDate() {
+      const day = this.date.getDate()
+      const month = this.date.getMonth() + 1
+      const year = this.date.getFullYear()
+      const dayStr = day < 10 ? '0' + day : day
+      const monthStr = month < 10 ? '0' + month : month
+      return `${year}-${monthStr}-${dayStr}`
     },
   },
   unmounted() {},
--- src/main.js
+++ src/main.js
@@ -3,13 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/styles.scss'
+import 'v-calendar/dist/style.css';
 import { focusDirective, clickOutside } from './directives'
-
+import { SetupCalendar } from 'v-calendar';
 const app = createApp(App)
 
 app.directive('focus', focusDirective)
 app.directive('click-outside', clickOutside)
 
+app.use(SetupCalendar, {})
 app.use(router)
 app.use(store)
 app.mount('#app')
