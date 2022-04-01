88fa8aee1ca21a33ec34cbfa7366165bd7331995
Roy Ben Aviv <roybenavivv@gmail.com>
2022-04-01 19:01:56 +0300

    PWA

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), VitePWA({
-
     includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],  
+    registerType: "autoUpdate",
     manifest: {
       name: 'Trellox',
       short_name: 'Trellox',
