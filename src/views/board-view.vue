87520967ea510ca7755fce642499d41a205780ef
Tamir <Tamirbelisha@gmail.com>
2022-04-02 13:48:08 +0300

    card edit responsive

--- src/views/board-view.vue
+++ src/views/board-view.vue
@@ -8,7 +8,7 @@
           <input v-else :style="'width:' + board.title.length * 12 + 'px'" class="custom-input, title-input" type="text" v-focus v-model="board.title" @keyup.enter="editBoardTitle" v-click-outside="() => editBoardTitle()" />
           <button class="star" :class="{ full: board.isStarred }" @click="updateKey('isStarred', 'toggle')"></button>
           <span class="seperator">|</span>
-          <container style="height: 32px" class="members-container" orientation="horizontal" group-name="3" :get-child-payload="getChildPayload">
+          <container v-if="members.length > 1" style="height: 32px" class="members-container" orientation="horizontal" group-name="3" :get-child-payload="getChildPayload">
             <draggable
               @mousedown="this.$store.commit({ type: 'memberDrag', isDrag: true })"
               style="height: 32px"
