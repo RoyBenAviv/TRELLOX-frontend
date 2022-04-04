b8d00407c5f850807cb430090f6100602db445ed
shani kupiec <shani.kupiec@gmail.com>
2022-04-04 19:29:36 +0300

    dfg

--- src/components/board/card-preview.vue
+++ src/components/board/card-preview.vue
@@ -30,7 +30,7 @@
           <div v-else class="card-preview-cover-color" :style="`background-color: ${card.style.cover};`"></div>
         </div>
         <img class="card-image" v-if="card.attachments.length && !card.style.cover && card.attachments[0].format !== 'mp4'" :src="card.attachments[0].url" />
-        <video-player v-if="card.attachments[0].format == 'mp4'" :options="videoOptions(card.attachments[0].url)" />
+        <video-player v-if="card.attachments.length && !card.style.cover && card.attachments[0].format == 'mp4'" :options="videoOptions(card.attachments[0].url)" />
         <div :style="(card.style.type === 'url' && !card.style.fullCover) || (card.attachments.length && !card.style.cover && !card.style.fullCover) ? 'margin-top: unset' : ''" class="card-label-container">
           <span v-for="label in labels" :key="label.id" @click.stop="toggleLabelTitle" :class="[label.className, labelTitleShown]" class="card-label" :title="label.title">
             <span v-if="labelTitleShown">{{ label.title }}</span>
